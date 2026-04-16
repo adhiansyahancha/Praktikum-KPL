242093e7173cc7885bb362e9dbe0e01b50d59bdc
Adhiansyah Ancha <69331906+adhiansyahancha@users.noreply.github.com>
2026-04-16 13:04:52 +0700

    feat: tambah 07

---
+++ 07_Grammar_Based_Text_Processing/index.js
@@ -0,0 +1,11 @@
+/**
+ * JSDoc ini opsional mau dibuat cek ketat atau tidak.
+ * Boleh dihapus, boleh dibuat ketat.
+ * @param {string} text Teks yang diambil dari berkas
+ * @returns {import('./structure').RobotsTxt} 
+ */
+function parseRobots(text) {
+    // TODO
+}
+
+module.exports = parseRobots;
